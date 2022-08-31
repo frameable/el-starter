60628662bae7b5392fb71d70903daba30aefe7f2
David Chester <david@frameable.com>
2022-08-31 13:23:55 +0000

    move app-el to its own file

---
+++ README.md
@@ -0,0 +1,15 @@
+# El Starter
+
+Minimal template / starter app scaffolding with @frameable/el
+
+### Running the app
+
+In development...
+
+```
+npm install
+npm start:dev
+```
+
+Then visit http://localhost:3000 or thereabouts
+
--- src/index.js
+++ src/index.js
@@ -1,34 +1,3 @@
 import { El } from '@frameable/el'
 import './components/index.js'
-
-class AppEl extends El {
-
-  styles(css) {
-    return css`
-      :host {
-        display: flex;
-        flex-direction: column;
-        flex: 1;
-        min-height: 100vh;
-      }
-      main {
-        background: #f3f5f8;
-        display: grid;
-        flex: 1;
-        place-items: center;
-      }
-    `
-  }
-
-  render(html) {
-    return html`
-      <header-el></header-el>
-      <main>
-        <greetying-cycler-el><greeting-cycler-el>
-      </main>
-      <footer-el></footer-el>
-    `
-  }
-}
-
-customElements.define('app-el', AppEl)
+import './app-el.js'
